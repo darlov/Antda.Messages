1c6701c78ed7bab8ccbcea77cb934c7ecc6cdf93
Dzianis Arlouski <tranquilfirst@gmail.com>
2025-04-04 23:09:42 +0300

    Fix build

--- src/benchmarks/Antda.Messages.Benchmarks/MessageSenderBenchmark.cs
+++ src/benchmarks/Antda.Messages.Benchmarks/MessageSenderBenchmark.cs
@@ -1,9 +1,6 @@
 ﻿using Antda.Messages.Benchmarks.Handlers;
-using Antda.Messages.Extensions;
 using Antda.Messages.Extensions.Microsoft.DependencyInjection;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Diagnostics.Windows.Configs;
-using BenchmarkDotNet.Jobs;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -20,8 +17,8 @@ public class MessageSenderBenchmark
   public MessageSenderBenchmark()
   {
     var serviceCollection = new ServiceCollection();
-    serviceCollection.AddAntdaMessages(cfg => cfg.RegisterHandlersFromAssembly(typeof(BaseMessageHandler).Assembly));
-    serviceCollection.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining(typeof(MessageSenderBenchmark)));
+    serviceCollection.AddAntdaMessages(cfg => cfg.RegisterHandlersFromAssembly<BaseMessageHandler>());
+    serviceCollection.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<MessageSenderBenchmark>());
     _serviceProvider = serviceCollection.BuildServiceProvider();
     _message = new BaseMessage();
   }
@@ -29,20 +26,20 @@ public class MessageSenderBenchmark
   [Benchmark]
   public async Task MediatR()
   {
-    var _ = await _serviceProvider.GetRequiredService<IMediator>().Send(_message);
+    _ = await _serviceProvider.GetRequiredService<IMediator>().Send(_message);
   }
   
   
   [Benchmark]
   public async Task MessageSender()
   {
-    var _ = await _serviceProvider.GetRequiredService<IMessageSender>().SendAsync(_message);
+    _ = await _serviceProvider.GetRequiredService<IMessageSender>().SendAsync(_message);
   }
 
   [Benchmark(Baseline = true)]
   public async Task DirectCall()
   {
-    var _ = await _serviceProvider.GetRequiredService<IMessageHandler<BaseMessage, string>>().HandleAsync(_message, CancellationToken.None);
+    _ = await _serviceProvider.GetRequiredService<IMessageHandler<BaseMessage, string>>().HandleAsync(_message, CancellationToken.None);
   }
   
 
